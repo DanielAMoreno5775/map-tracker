c5d2c3ee484747126e664b689f4e4039942c54df
Moreno, Daniel <danielmoreno3@my.unt.edu>
2023-10-24 15:47:50 -0500

    Link to Amazon Cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId:         'us-east-2_VSxN77djB',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '5dvlj9atvaemnot5a6qb9rgkpq',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
